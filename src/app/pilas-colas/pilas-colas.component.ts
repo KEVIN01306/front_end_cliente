e160af543af63d51c5fe1970258d25e826e02070
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-04-17 11:45:32 -0600

    commit-data-tables

--- src/app/pilas-colas/pilas-colas.component.ts
+++ src/app/pilas-colas/pilas-colas.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
-import { IonChip} from '@ionic/angular/standalone';
+
+
+import { Component, OnInit, OnDestroy } from '@angular/core'; // Importa OnDestroy
+import { Config, IonChip} from '@ionic/angular/standalone';
+import { MatCardModule } from '@angular/material/card';
+import { DataTablesModule } from 'angular-datatables';
+import { Subject } from 'rxjs'; // Importa Subject para la destrucción
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-pilas-colas',
   templateUrl: '../pilas-colas/pilas-colas.component.html',
   styleUrls: ['../pilas-colas/pilas-colas.component.scss'],
-  standalone: true, 
-  imports: [IonChip],
+  standalone: true,
+  imports: [IonChip, MatCardModule, DataTablesModule, CommonModule], // Agrega CommonModule aquí
 })
-export class PilasColasComponent{
-  tituloPila = 'Información sobre Pilas';
-  contenidoPila = 'Aquí puedes encontrar detalles y ejemplos sobre la estructura de datos de Pilas (Stacks).';
+export class PilasColasComponent implements OnInit, OnDestroy {
+  dtoptions: any = {};
+  dtTrigger: Subject<any> = new Subject<any>();
 
+  data: any[] = [
+    { columna1: 'Dato 1', columna2: 'Dato A', columna3: 10, columna4: true },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dtoptions = {
+      pagingType: 'full'
+      // Puedes agregar más opciones de DataTables aquí
+    };
+    this.dtTrigger.next(null);
+  }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
 }
