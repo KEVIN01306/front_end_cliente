22bc144da96ab8faa34f0f92c7e0bdd19c63663a
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-04-18 23:36:54 -0600

    commit-carrusel

--- src/app/info-pilas-colas/info-pilas-colas.component.ts
+++ src/app/info-pilas-colas/info-pilas-colas.component.ts
@@ -1,13 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, AfterViewInit, Renderer2, ElementRef, ViewChildren, QueryList, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import {IonButtons, IonMenuButton,
-        IonContent,
-        IonButton,
-        IonTitle,
-        IonToolbar,
-        IonHeader,
-        IonBreadcrumb, IonBreadcrumbs,
-        IonModal,IonCol, IonGrid, IonRow
+import { IonButtons, IonMenuButton,
+  IonContent,
+  IonButton,
+  IonTitle,
+  IonToolbar,
+  IonHeader,
+  IonBreadcrumb, IonBreadcrumbs,
+  IonModal,IonCol, IonGrid, IonRow
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -22,26 +22,52 @@ import {IonButtons, IonMenuButton,
     IonHeader,
     IonModal,IonCol, IonGrid, IonRow,IonBreadcrumb, IonBreadcrumbs],
 })
-export class InfoPilasColasComponent implements OnInit {
+export class InfoPilasColasComponent implements OnInit, AfterViewInit  {
   public category: string | null = null;
   public type: string | null = null;
   public id: string | null = null;
+
+  @ViewChild('grandeElement') grandeElement!: ElementRef;
+  @ViewChildren('puntoElement') puntoElements!: QueryList<ElementRef>;
+
   private activatedRoute = inject(ActivatedRoute);
-  constructor(private router: Router) {}
+
+  constructor(private router: Router, private renderer: Renderer2) {}
 
   ngOnInit() {
     this.category = this.activatedRoute.snapshot.paramMap.get('category') as string;
     this.id = this.activatedRoute.snapshot.paramMap.get('id') as string;
     this.type = this.activatedRoute.snapshot.paramMap.get('type') as string;
-    console.log(this.id,this.type,this.category)
+    console.log(this.id,this.type,this.category);
   }
 
-  returnTable() { // Explicitly type the parameters
+  ngAfterViewInit() {
+    if (this.grandeElement) {
+      const grande = this.grandeElement.nativeElement;
+
+      this.puntoElements.forEach((puntoElement, i) => {
+        const punto = puntoElement.nativeElement;
+        this.renderer.listen(punto, 'click', () => {
+          let posicion = i;
+          let operacion = posicion * -10;
+          this.renderer.setStyle(grande, 'transform', `translateX(${operacion}%)`);
+
+          this.puntoElements.forEach((cadaPunto) =>{
+            const puntoIn = cadaPunto.nativeElement;
+            this.renderer.removeClass(puntoIn,'activo')
+          })
+
+          this.renderer.addClass(punto,'activo');
+        });
+      });
+    }
+  }
+
+  returnTable() {
     this.router.navigate([`/folder/${this.category}`]);
   }
 
-  here() { // Explicitly type the parameters
+  here() {
     this.router.navigate([`/folder/${this.category}/${this.type}/${this.id}`]);
   }
 }
-
